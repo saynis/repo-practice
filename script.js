1d1cf033673c6a5d4955d6993616d386172d3679
naimamahamed <niciimama6@gmail.com>
2026-09-12 21:30:35 +0300

    waxaan ku daray file script ah

---
+++ script.js
@@ -0,0 +1,10 @@
+let per = 67
+if(per >90){
+    console.log("A+")
+    }else if(per >70){
+        console.log("B+")
+    }else if (per >50){
+        console.log("C+")
+    }else(
+        console.log("failed")
+    )
